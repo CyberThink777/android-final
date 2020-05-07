675df278b8cb691121e88429896ee8ad01a30780
CyberThink777 <alexanderm61@gmail.com>
2020-05-07 13:42:58 +0700

    Migrate Tests to java

---
+++ app/src/androidTest/java/org/mantap/finalcuk/InstrumentedTest.java
@@ -0,0 +1,25 @@
+package org.mantap.finalcuk;
+
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4.class)
+class InstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("org.mantap.finalcuk", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/org/mantap/finalcuk/UnitTest.java
@@ -0,0 +1,17 @@
+package org.mantap.finalcuk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class UnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
