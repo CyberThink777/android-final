036920cd08263fe99a1137698809487cc1d18528
CyberThink777 <alexanderm61@gmail.com>
2020-05-09 08:53:35 +0700

    Change Tabs into BottomNavBar

--- app/src/main/java/org/mantap/finalcuk/MainActivity.java
+++ app/src/main/java/org/mantap/finalcuk/MainActivity.java
@@ -3,6 +3,10 @@ package org.mantap.finalcuk;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView botNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(botNav, navController);
     }
 }
--- app/src/main/java/org/mantap/finalcuk/VideoListFragment.java
+++ app/src/main/java/org/mantap/finalcuk/VideoListFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class VideoListFragment extends Fragment {
-    @Nullable
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_video, container, false);
